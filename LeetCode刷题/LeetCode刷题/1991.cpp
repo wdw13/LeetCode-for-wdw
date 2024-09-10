c1b7cb2101a020547c85d534d4ca535c6a852a48
wdw <1184749523@qq.com>
2024-09-10 19:55:25 +0800

    2024.9.10

---
+++ LeetCode刷题/LeetCode刷题/1991.cpp
@@ -0,0 +1,24 @@
+﻿//1991 简单
+//给你一个整数数组 nums ，请计算数组的 中心下标 。
+//数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+//如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+//如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 - 1 。
+class Solution {
+public:
+	int pivotIndex(vector<int>& nums) {
+		if (nums.empty()) {
+			return -1;
+		}
+		int left = 0;
+		int acc = accumulate(nums.begin(), nums.end(), 0);
+		for (int i = 0; i < nums.size(); ++i) {
+			if (left * 2 + nums[i] == acc) {
+				return i;
+			}
+			else {
+				left += nums[i];
+			}
+		}
+		return -1;
+	}
+};
---
+++ LeetCode刷题/LeetCode刷题/LC 合并区间.cpp
@@ -0,0 +1,27 @@
+﻿//LC 合并区间
+//以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+//请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+class Solution {
+public:
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		int n = intervals.size();
+		if (n == 1) {
+			return intervals;
+		}
+		sort(intervals.begin(), intervals.end());
+		int j = 0;
+		vector<vector<int>> m;
+		m.push_back(intervals[0]);
+		for (int i = j + 1; i < intervals.size(); ++i) {
+			if (intervals[i][0] <= m[j][1]) {
+				m[j][1] = max(m[j][1], intervals[i][1]);
+			}
+			else {
+				m.push_back(intervals[i]);
+				j++;
+			}
+		}
+		return m;
+	}
+};
